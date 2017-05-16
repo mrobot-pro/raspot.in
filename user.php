0ccde29977c8c17fd7eb631de482614f6fc65559
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-05-16 15:21:54 +0200

    ow

--- user.php
+++ user.php
@@ -1,7 +1,11 @@
 <?php
 
-
-
+//Affiche les bases dispos
+echo '<h3>Bases disponibles</h3>';
+foreach (PDO::getAvailableDrivers() as $pro_driver) {
+    echo 'disponible: '.$pro_driver . "<br/>";
+}
+echo '<h3>Tables user</h3>';
 $dbh=new PDO("sqlite:snapspot.sqlite");
 /* Vérification de la connexion */
 
@@ -9,12 +13,33 @@ $res = $dbh->query("SELECT *
 FROM user");
 
 if ($res != false){ 
+
         while($row = $res->fetch()){
             echo $row['user_id'].' '.$row['nom'].' '.$row['prenom']."<br/>";
+			
+        }
+		
+} else{
+        echo "erreur de connexion";
+}
+
+
+echo '<h3>Tables media</h3>';
+$dbh=new PDO("sqlite:snapspot.sqlite");
+/* Vérification de la connexion */
+
+$res = $dbh->query("SELECT * 
+FROM media");
+
+if ($res != false){ 
+
+        while($row = $res->fetch()){
+            echo $row['media_id'].' '.$row['chemin'].' '.$row['description']."<br/>";
         }
 } else{
         echo "erreur de connexion";
 }
+
 
 /* Fermeture de la connexion */
 unset($dbh);
