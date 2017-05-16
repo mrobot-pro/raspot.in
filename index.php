23957ba00be365694744f541dce3ced4608831ac
DavidLdnr <davidalexandre.fournier.pro@gmail.com>
2017-05-16 10:15:15 +0200

    maj index

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ echo <<< EOT
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Bibliothèque multim&eacute;dia</title>
+		<title>SnapSpot</title>
 		<link rel="stylesheet" href="./css/default.css" title = "default">
     
 		<script type="text/javascript" src="js/affichage.js"></script>
@@ -27,10 +27,13 @@ echo '<header>'."\n";
 echo '<h1><a href="index.php">SNAP SNAP SPOT !</a></h1>'."\n";
 echo '</header>'."\n";
 ?>
-<div id="explain">Partagez vos photos sur ce spot tout au long de la soirée !</div>
+
 <!--Solution 1-->
 <img src="./image/favicon.png" id="upfile1" style="cursor:pointer" />
 <input type="file" id="file1"  name="file1" style="display:none" />
+
+<div id="explain">Partagez vos photos sur ce spot tout au long de l'événement' !</div>
+
 <script type="text/javascript" src="js/jquery-3.2.1.js" > </script>
 <script type="text/javascript" src="js/snapspot.js" > </script>
 
