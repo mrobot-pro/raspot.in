180de3964d4deb9a809559b8b0f8c4cacd50db9c
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-05-19 10:39:05 +0200

    ow

--- user.php
+++ user.php
@@ -2,37 +2,36 @@
 
 //Affiche les bases dispos
 echo '<h3>Bases disponibles</h3>';
-foreach (PDO::getAvailableDrivers() as $pro_driver) {
-    echo 'disponible: '.$pro_driver . "<br/>";
+foreach (PDO::getAvailableDrivers() as $pdo_driver) {
+    echo 'disponible: '.$pdo_driver . "<br/>";
 }
 echo '<h3>Tables user</h3>';
+/* Connexion à la base */
 $dbh=new PDO("sqlite:snapspot.sqlite");
-/* Vérification de la connexion */
 
+/* requete sql */
 $res = $dbh->query("SELECT * 
 FROM user");
 
+/* boucle d'affichage users */
 if ($res != false){ 
-
         while($row = $res->fetch()){
             echo $row['user_id'].' '.$row['nom'].' '.$row['prenom']."<br/>";
-			
         }
-		
 } else{
         echo "erreur de connexion";
 }
 
-
 echo '<h3>Tables media</h3>';
+/* Connexion à la base */
 $dbh=new PDO("sqlite:snapspot.sqlite");
-/* Vérification de la connexion */
 
+/* requete sql */
 $res = $dbh->query("SELECT * 
 FROM media");
 
+/* boucle d'affichage medias */
 if ($res != false){ 
-
         while($row = $res->fetch()){
 			$src=$row['chemin'];
             echo $row['media_id'].' '.$src.' '.$row['description']."<br/>".'<a href="'.$src.'"><img src="'.$src.'" height="100"></a>'."<br/>";
@@ -40,7 +39,6 @@ if ($res != false){
 } else{
         echo "erreur de connexion";
 }
-
 
 /* Fermeture de la connexion */
 unset($dbh);
