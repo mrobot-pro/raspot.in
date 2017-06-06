0af94be35bcc3b39ce3f91f1e0f74144ccc37493
DavidLdnr <davidalexandre.fournier.pro@gmail.com>
2017-06-06 09:58:04 +0200

    depot fichiers olivier 060617

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+
+// AUTOLOAD //
+require('php/autoload.php');
+
+
+ // APPELLE DE SESSION //
+session_start(); // On appelle session_start() APRÈS avoir enregistré l'autoload.
+ 
+    // CONNEXION SQLITE //
+$db = new PDO('sqlite:snapspot.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
+
+//  NOUVELLE INSTANCE DE LA CLASS MANAGER = OBJET $manager //
+$manager = new MediaManager($db);
+
+// TABLEAU DES ROWS PRESENTS DANS LA BASE //
+$medias = $manager->getList();
+
+echo '<p>Nombre de photos : '.$manager->count().'</p><br>';
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+    <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SnapSpot</title>
+	<link rel="stylesheet" href="./css/default.css" title = "default">
+    </head>
+    <body>
+
+        <header>
+        <center><h1><a href="index.php">Snap Snap Spot !</a></h1></center>
+        <!-- <center><img src="./image/favicon.png" alt="logo_appli" /></center> -->
+        </header>
+      
+<form method="post" id='uploadform' action="index.php" enctype="multipart/form-data">
+<center><img src="./css/camera.ico" id="upfile1" width="75" style="cursor:pointer" image-orientation="center"  alt="logo_appli" type="submit" />
+<!-- MAX_FILE_SIZE doit précéder le champ input de type file 
+<input type="hidden" name="MAX_FILE_SIZE" value="30000" /> -->
+<input type="file" id="mon_fichier"  name="mon_fichier" style="display:none"  />
+</form>
+    <?php
+
+if (isset($_FILES['mon_fichier'])){
+    //Vérification image uploadée
+if ($_FILES['mon_fichier']['error'] > 0) {
+$erreur = "Erreur lors du transfert";
+}
+    
+    //  NOUVELLE INSTANCE DE LA CLASS MEDIA = OBJET $media
+$media = new Media([
+      'oldName' => $_FILES["mon_fichier"]["name"],                          
+      'fileSize' => $_FILES["mon_fichier"]["size"], 
+    
+     ]);
+$media->setNewName(md5(uniqid(rand(), true)).'.'.$media->getExtension());
+$media->saveFile();
+
+$vignette= Media::VIGN_PATH.$media->newName();
+$manager->add($media);
+}
+?> 
+
+
+
+ 
+        <div>
+	<p>Partagez vos photos </p>
+	<p>sur ce spot </p>
+	<p>tout au long de l'événement !</p>
+</div></center>
+<center><h1>BONJOUR</h1></center>
+        <footer>
+            <center>
+		<p>&copy;2017 - David Fournier&nbsp;&amp;&nbsp;Olivier Welter.</p>
+            </center>
+        </footer>
+    </body>
+</html>
+  <script type="text/javascript" src="js/jquery-3.2.1.js" ></script>
+  <script type="text/javascript" src="js/snapspot.js" ></script>
---
+++ php/MediaManager.class.php
@@ -0,0 +1,77 @@
+<?php
+
+class MediaManager
+{
+  private $_db; // Instance de PDO
+  public function __construct($db)
+  {
+    $this->setDb($db);
+  }
+  
+  public function add(Media $media)
+  {
+    $q = $this->_db->prepare('INSERT INTO media(oldName, newName, fileSize, pseudo, commentaire) VALUES(:oldName, :newName, :fileSize, :pseudo, :commentaire)');
+    $q->bindValue(':oldName', $media->oldName());
+    $q->bindValue(':newName', $media->newName());
+    $q->bindValue(':fileSize', $media->fileSize());
+    $q->bindValue(':pseudo', $media->pseudo());
+    $q->bindValue(':commentaire', $media->commentaire());
+    
+    $q->execute();
+    $media->hydrate([
+     
+    ]);
+  }
+
+  public function count()
+  {
+    return $this->_db->query('SELECT COUNT(*) FROM media')->fetchColumn();
+  }
+  
+  public function delete(Media $media)
+  {
+    $this->_db->exec('DELETE FROM media WHERE id = '.$media->mediaId());
+  }
+  public function get($info)
+  {
+    if (is_int($info))
+    {
+      $q = $this->_db->query('SELECT mediaId, oldName, newName, mediaPath, fileSize, pseudo, commentaire FROM media WHERE mediaId = '.$info);
+      $donnees = $q->fetch(PDO::FETCH_ASSOC);
+      return new Media($donnees);
+    }
+    else
+    {
+      //$q = $this->_db->prepare('SELECT mediaId, oldName, newName, mediaPath, fileSize, pseudo, commentaire FROM media WHERE pseudo = :pseudo');
+      //$q->execute([':pseudo' => $info]);
+      //return new Media($q->fetch(PDO::FETCH_ASSOC));
+    }
+  }
+  
+  public function getList()
+  {
+    $medias = [];
+    $q = $this->_db->prepare('SELECT mediaId, oldName, newName, fileSize, pseudo, commentaire FROM media');
+    $q->execute();
+    while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+    {
+      $medias[] = new Media($donnees);
+    }
+    return $medias;
+  }
+  
+  public function update(Media $media)
+
+  {
+$q = $this->_db->prepare('UPDATE media SET pseudo = :pseudo, commentaire = :commentaire WHERE mediaId = :mediaId');
+$q->bindValue(':pseudo', $media->pseudo(), PDO::PARAM_INT);
+$q->bindValue(':commentaire', $media->commentaire(), PDO::PARAM_INT);
+$q->bindValue(':mediaId', $media->mediaId(), PDO::PARAM_INT);
+$q->execute();
+ }
+  
+  public function setDb(PDO $db)
+  {
+    $this->_db = $db;
+  }
+}
