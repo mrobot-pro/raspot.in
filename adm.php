89f0f6dc7e612858c3c5f912bf27f012744c64f4
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-06-09 15:46:33 +0200

    ok

--- adm.php
+++ adm.php
@@ -23,12 +23,90 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une aler
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>SNAPSPOT</title>
-		 <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		 <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
 	</head>
 
 	<body>
-		
-<div id="main-container" class="container-fluid">
+	
+    <div id="main-container" class="container-fluid">
+    <div class="text-center"><h1><a href="/" class="heading">Snap Snap Toast !</a></h1></div>
+<h3 class="heading text-center">Administration</h3>
+
+<form action="" id="admin-form" class="row" method="post" enctype="multipart/form-data">
+    <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-xs-12">
+        <br>
+        <label for="password">Changer le mot de passe de l'administrateur</label>
+        <div class="input-group">
+            <input id="password" class="form-control" type="password" name="password" value="" placeholder="*******"/>
+            <div class="input-group-btn">
+                <input type="submit" name="change_password" value="Modifier" class="btn btn-primary"/>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-xs-12">
+        <br>
+        <label for="title">Changer le titre</label>
+        <div class="input-group">
+            <input id="title" class="form-control" type="text" name="title"
+                   value="Snap Snap Toast !" placeholder="BetaBox!"/>
+            <div class="input-group-btn">
+                <input type="submit" name="change_title" value="Modifier" class="btn btn-primary"/>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-xs-12">
+        <br>
+        <label for="message">Changer le message d'explications</label>
+        <div class="input-group">
+            <textarea name="message" id="message" class="form-control custom-control" rows="3" style="resize:none"
+                      placeholder="Saisissez votre message...">Partagez vos photos sur ce spot tout au long de la soirée !</textarea>
+            <label class="input-group-addon btn btn-primary" for="hidden_submit">Modifier</label>
+        </div>
+    </div>
+    <input class="invisible" id="hidden_submit" type="submit" value="Modifier"
+           style="visibility: hidden; position: absolute; top: 0; left: 0; height: 0; width: 0;"/>
+    <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-xs-12">
+        <br>
+        <label for="message">Changer l'image de fond</label>
+        <div class="input-group">
+            <input type="file" name="file" class="form-control"/>
+            <div class="input-group-btn">
+                <input class="btn btn-primary" type="submit" name="image_submit" value="Envoyer"/>
+                            </div>
+        </div>
+    </div>
+    <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-xs-12">
+        <br>
+        <label for>
+            Utilisation du disque        </label>
+        <div class="row">
+                                        <div class="col-xs-12">
+                                        <div class="progress text-center"
+                         style="text-indent: -89%;">
+                        <div class="progress-bar progress-bar-success progress-bar-striped"
+                             role="progressbar"
+                             aria-valuemin="0" aria-valuemax="100"
+                             style="min-width: 2em;width: 89%;">
+                        </div>
+                        <span>89% / 14.0 Go disponible</span>
+                    </div>
+                </div>
+                    </div>
+    </div>
+    <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-xs-12">
+        <br>
+        <label for>Nettoyage du disque</label>
+        <div class="input-group-btn">
+                            <input class="btn btn-primary confirm-delete disabled"
+                       type="submit" name="delete_backup_submit"
+                       value="Vider les sauvegardes (0)"/>
+            <input class="btn btn-primary confirm-delete disabled"
+                   type="submit" name="delete_photos_submit"
+                   value="Vider les images (0)"/>
+        </div>
+    </div>
+</form>
+</div>
 
 
 <?php
@@ -138,13 +216,12 @@ else
 }
 }
 ?>
-</div>
 		<footer>
             <center>
 		<p>&copy;2017 - David Fournier&nbsp;&amp;&nbsp;Olivier Welter.</p>
             </center>
         </footer>
-  <script type="text/javascript" src="js/jquery-3.2.1.js" ></script>
+  <script type="text/javascript" src="js/jquery.js" ></script>
   <script type="text/javascript" src="js/snapspotAdm.js" ></script>
 	</body>
 </html>
