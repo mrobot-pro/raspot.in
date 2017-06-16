17deddf5e0645d7cd8bf27b8fbb1b1d1639119f2
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-06-16 09:44:40 +0200

    ok

--- Gabarit.php
+++ Gabarit.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SNAPSPOT ADMIN</title>
+   <title><?= $titre ?></title>   <!-- Élément spécifique -->
     <link href="css/default.css" rel="stylesheet">
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
   </head>
--- Modele.php
+++ Modele.php
@@ -2,8 +2,15 @@
 
 // Renvoie la liste de tous les billets, triés par identifiant décroissant
 function getMedias() {
-    $db = new PDO('sqlite:snapspot.sqlite');
+    $db = getDb();
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
     $medias = $db->query('select * from media');
-    return $medias;
+    $datas = $medias->fetchAll(PDO::FETCH_ASSOC);
+    return $datas;
+}
+// Effectue la connexion à la BDD
+// Instancie et renvoie l'objet PDO associé
+function getDb() {
+  $db = new PDO('sqlite:snapspot.sqlite');
+  return $db;
 }
