2edfc017899eca29087e86d002046999224c203f
DavidLdnr <davidalexandre.fournier.pro@gmail.com>
2017-06-26 22:24:36 +0200

    changement titre vue accueil et contrainte input type

--- Vue/vueAccueil.php
+++ Vue/vueAccueil.php
@@ -1,4 +1,4 @@
-<?php $this->titre = "Snap Snap Spot"; ?>
+<?php $this->titre = "Raspot.in"; ?>
 
             <div class="row">
                 <div class="row text-center">
@@ -7,7 +7,7 @@
                             <img class="cameraico" src="contenu/css/lens-37025_640.png" id="upfile1" width="75" style="cursor:pointer" alt="logo_appli" type="submit" />
                             <!-- MAX_FILE_SIZE doit précéder le champ input de type file
                             <input type="hidden" name="MAX_FILE_SIZE" value="30000" />-->
-                            <input type="file" id="mon_fichier"  name="mon_fichier" style="display:none"  />
+                            <input type="file" id="mon_fichier"  name="mon_fichier" accept="image/*" capture="user" style="display:none"  />
                         </form>
 
                         <div id="explain">
