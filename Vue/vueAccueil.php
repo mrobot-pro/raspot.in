b1861f0e5f661817a953fccc75339097b34a4fa6
DavidLdnr <davidalexandre.fournier.pro@gmail.com>
2017-06-26 17:09:24 +0200

    changement animation et photo libre de droit sur pixabay

--- Vue/vueAccueil.php
+++ Vue/vueAccueil.php
@@ -1,16 +1,16 @@
 <?php $this->titre = "Snap Snap Spot"; ?>
 
-            <div class="row text-center">
-                <div class="col-xs-12">
-                    <form method="post" id='uploadform' action="index.php" enctype="multipart/form-data">
-                        <img class="cameraico" src="contenu/css/camera.ico" id="upfile1" width="75" style="cursor:pointer" alt="logo_appli" type="submit" />
-                        <!-- MAX_FILE_SIZE doit précéder le champ input de type file
-                        <input type="hidden" name="MAX_FILE_SIZE" value="30000" />-->
-                        <input type="file" id="mon_fichier"  name="mon_fichier" style="display:none"  />
-                    </form>
+            <div class="row">
+                <div class="row text-center">
+                    <div class="col-xs-12">
+                        <form method="post" id='uploadform' action="index.php" enctype="multipart/form-data">
+                            <img class="cameraico" src="contenu/css/lens-37025_640.png" id="upfile1" width="75" style="cursor:pointer" alt="logo_appli" type="submit" />
+                            <!-- MAX_FILE_SIZE doit précéder le champ input de type file
+                            <input type="hidden" name="MAX_FILE_SIZE" value="30000" />-->
+                            <input type="file" id="mon_fichier"  name="mon_fichier" style="display:none"  />
+                        </form>
 
-                    <div id="explain">
-
+                        <div id="explain">
 <?php     
     
     $parametres = new Parametres();
@@ -25,4 +25,5 @@
 
                     </div>
                 </div>
+            </div>
             </div>
