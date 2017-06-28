f99ee9327377b5306001958fd97123ff9281d944
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-06-28 18:44:06 +0200

    autofocus sur l'input mdp de connexion

--- Vue/vueConnexion.php
+++ Vue/vueConnexion.php
@@ -8,7 +8,7 @@
         <div class="col-lg-offset-4 col-lg-4 col-md-offset-2 col-md-8 col-xs-12">
                 <label for='password'>Mot de passe</label><br>
                 <div class="input-group">
-                        <input id='password' class='form-control' type='password' name='mdp' required>
+                        <input id='password' class='form-control' type='password' name='mdp' required autofocus>
                         <div class="input-group-btn">
                                 <input class="btn btn-primary" type="submit" name="connexion" value="Valider">
                                 </div>
