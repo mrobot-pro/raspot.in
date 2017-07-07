01205b337d1536eb8ad38f06763a4e3f5509ea3d
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-07-07 15:12:43 +0200

    ok

--- testfiles/controller/ConnexionControllerTest.php
+++ testfiles/controller/ConnexionControllerTest.php
@@ -41,16 +41,5 @@ class ConnexionControllerTest extends PHPUnit_Framework_TestCase
         );
     }
 
-    /**
-     * @covers ConnexionController::redirect
-     * @todo   Implement testRedirect().
-     */
-    public function testRedirect()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
-    }
 
 }
